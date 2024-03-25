22a435508213555e01ad2ea194c2c93c2d676cea
Pedro Lesbrão Progamador <90758356+PedroLesbrao@users.noreply.github.com>
2024-03-25 17:23:36 -0300

    Add files via upload

---
+++ base.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <title>Document</title>
+        <link type="text/css" rel="stylesheet" href="estilo.css"/>
+    </head>
+    <body>
+        <h1>CT Desenvolvimento de Sistemas Back-End</h1>
+        <div id="container">
+            <?php
+                
+            ?>
+        </div>
+    </body>
+</html>
